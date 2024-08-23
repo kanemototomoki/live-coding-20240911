af5707f483c137859b0bc433b3929420aa32eb58
kanemoto-tomoki_cyg <kanemoto_tomoki@external.cygames.jp>
2024-08-23 17:39:25 +0900

    update: page

--- src/app/media-query/page.tsx
+++ src/app/media-query/page.tsx
@@ -1,3 +1,23 @@
+import type { Metadata, ResolvingMetadata } from "next";
+
+const title = "Media Query Page";
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const parentMetadata = await parent;
+  const parentTitle = parentMetadata.title?.absolute;
+
+  return {
+    title: `${title} - ${parentTitle}`,
+  };
+}
+
 export default function Page() {
-  return <h1>Media Query</h1>;
+  return <h1>{title}</h1>;
 }
