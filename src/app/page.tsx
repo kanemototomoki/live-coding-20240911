173765a67a931f84c2dd5ad945e676773321d000
kanemototomoki <kanemototomoki@gmail.com>
2024-09-11 13:21:30 +0900

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-import styles from "./page.module.css";
-import Link from "next/link";
-
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/media-query">Media Query</Link>
-          </li>
-          <li>
-            <Link href="/container-query">Container Query</Link>
-          </li>
-        </ul>
-      </nav>
-    </main>
-  );
+  return <main className=""></main>;
 }
