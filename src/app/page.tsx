88a7a9fbd6faf02267bbb9174ca3e15e4962e36b
kanemototomoki <kanemototomoki@gmail.com>
2024-09-11 14:53:41 +0900

    update: 2 section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="main">
+      <div className="fv">
+        <div className="wrapper">
+          <h1 className="title">Bolt Theme</h1>
+          <p className="sub-title">FREE BOOTSTRAP THEME</p>
+        </div>
+      </div>
+      <div className="what-we-can-do">
+        <div className="wrapper">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/iphone.png" alt="" />
+          <div className="content">
+            <h2 className="title"> WHAT WE CAN DO?</h2>
+            <p className="description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
